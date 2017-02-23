0666982a2e211dd5b66ac6537682b1388541f217
Ignacio Lorenzo García <nacho@oag-fundacion.org>
2017-02-23 08:51:59 +0000

    Cambia forma de crear servicios

--- buoy/lib/daemon/daemon.py
+++ buoy/lib/daemon/daemon.py
@@ -1,141 +1,37 @@
 """Generic linux daemon base class for python 3.x."""
 
-import atexit
 import logging
-import os
-import signal
-import sys
 import time
+
+from functools import wraps
+from buoy.lib.device.base import Device
 
 logger = logging.getLogger(__name__)
 
 
 class Daemon(object):
-    """A generic daemon class.
+    def __init__(self, device: Device) -> None:
+        self.device = device
 
-    Usage: subclass the daemon class and override the run() method."""
+    def __start(self, f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            self.device.connect()
+            f(*args, **kwargs)
+            while True:
+                time.sleep(5)
 
-    def __init__(self, name, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
-        self.stdin = stdin
-        self.stdout = stdout
-        self.stderr = stderr
-        self.name = name
-        self.pidfile = '/var/run/' + name + '.pid'
+        return wrapped
 
-    def daemonize(self):
-        """Deamonize class. UNIX double fork mechanism."""
-
-        try:
-            pid = os.fork()
-            if pid > 0:
-                # exit first parent
-                sys.exit(0)
-        except OSError as err:
-            sys.stderr.write('fork #1 failed: {0}\n'.format(err))
-            sys.exit(1)
-
-        # decouple from parent environment
-        os.chdir('/')
-        os.setsid()
-        os.umask(0)
-
-        # do second fork
-        try:
-            pid = os.fork()
-            if pid > 0:
-                # exit from second parent
-                sys.exit(0)
-        except OSError as err:
-            sys.stderr.write('fork #2 failed: {0}\n'.format(err))
-            sys.exit(1)
-
-        # redirect standard file descriptors
-        sys.stdin.flush()
-        sys.stdout.flush()
-        sys.stderr.flush()
-
-        si = open(self.stdin, 'r')
-        so = open(self.stdout, 'ab')
-        se = open(self.stderr, 'wb', 0)
-
-        #os.dup2(si.fileno(), sys.stdin.fileno())
-        #print("llgeo 1")
-        #os.dup2(so.fileno(), sys.stdout.fileno())
-        #print("llgeo 2")
-        #os.dup2(se.fileno(), sys.stderr.fileno())
-        #print("llgeo 3")
-
-        # write pidfile
-        atexit.register(self.delpid)
-
-        pid = str(os.getpid())
-        with open(self.pidfile, 'w+') as f:
-            f.write(pid + '\n')
-
-    def delpid(self):
-        os.remove(self.pidfile)
-
+    @__start
     def start(self):
-        """Start the daemon."""
-        logger.info("Starting service")
-
-        # Check for a pidfile to see if the daemon already runs
-        try:
-            with open(self.pidfile, 'r') as pf:
-                pid = int(pf.read().strip())
-        except IOError:
-            pid = None
-
-        if pid:
-            message = "pidfile {0} already exist. Service already running?\n".format(self.pidfile)
-            logger.warning(message)
-            sys.stderr.write(message)
-            sys.exit(1)
-
-        # Start the daemon
-        self.daemonize()
-        self.run()
+        pass
 
     def stop(self):
-        """Stop the daemon."""
-        logger.info("Stopping service")
-
-        # Get the pid from the pidfile
-        try:
-            with open(self.pidfile, 'r') as pf:
-                pid = int(pf.read().strip())
-        except IOError:
-            pid = None
-
-        if not pid:
-            message = "pidfile {0} does not exist. Service not running?\n".format(self.pidfile)
-            logger.warning(message)
-            sys.stderr.write(message.format(self.pidfile))
-            return  # not an error in a restart
-
-        # Try killing the daemon process
-        try:
-            while 1:
-                os.kill(pid, signal.SIGTERM)
-                time.sleep(0.1)
-        except OSError as err:
-            e = str(err.args)
-            if e.find("No such process") > 0:
-                if os.path.exists(self.pidfile):
-                    os.remove(self.pidfile)
-            else:
-                logger.error("Failed to kill the service", exc_info=True)
-                sys.exit(1)
+        self.device.disconnect()
 
     def restart(self):
-        """Restart the daemon."""
-        logger.info("Restarting service")
         self.stop()
         self.start()
 
-    def run(self):
-        """You should override this method when you subclass Daemon.
-
-        It will be called after the process has been daemonized by
-        start() or restart()."""
 
