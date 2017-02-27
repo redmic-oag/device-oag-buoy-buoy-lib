9283e7c5d70dce88f1573ea5a0efba2efbbd9980
Ignacio Lorenzo García <nacho@oag-fundacion.org>
2017-02-27 12:01:53 +0000

    Crea funciones para leer config

--- buoy/lib/utils/config.py
+++ buoy/lib/utils/config.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import configparser
 import yaml
 import logging
 from os import path
@@ -8,19 +7,33 @@ from os import path
 logger = logging.getLogger(__name__)
 
 
-def load_config_devices(path_config='/etc/buoy/buoy.cfg'):
+def load_config(path_config):
     if not path.isfile(path_config):
         logger.error("No exists config file %s" % (path_config,))
 
-    config = configparser.ConfigParser()
-    config.read(path_config)
+    config = yaml.load(open(path_config, 'r'))
 
     return config
 
 
-def load_config_logger(filename, config='/etc/buoy/logging.yaml'):
+def load_config_device(device_name, path_config='/etc/buoy/buoy.yaml'):
+
+    config = load_config(path_config=path_config)
+
+    return config['device'][device_name]
+
+
+def load_config_device_serial(device_name, path_config='/etc/buoy/device.yaml'):
+
+    config = load_config_device(device_name, path_config=path_config)
+
+    return config['device'][device_name]['serial']
+
+
+def load_config_logger(filename, path_config='/etc/buoy/logging.yaml'):
+    config = load_config(path_config=path_config)
+
     filename += '.log'
-    config = yaml.load(open(config, 'r'))
     config['handlers']['file']['filename'] = path.join(config['handlers']['file']['filename'], filename)
 
     return config
