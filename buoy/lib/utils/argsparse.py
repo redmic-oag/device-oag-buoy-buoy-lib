8a01169ce10856d3fcee09755371cf9e5764dd50
Ignacio Lorenzo García <nacho@oag-fundacion.org>
2017-03-02 12:35:09 +0000

    añade función para parsear args

--- buoy/lib/utils/argsparse.py
+++ buoy/lib/utils/argsparse.py
@@ -21,3 +21,13 @@ def parse_args():
                         default='/etc/buoy/logging.yaml', type=is_valid_file)
 
     return parser.parse_args()
+
+
+def parse_args_sender():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config-file", help="Ruta al fichero de configuración del servicio",
+                        default='/etc/buoy/server.yaml', type=is_valid_file)
+    parser.add_argument("--config-log-file", help="Ruta al fichero de configuración de los logs",
+                        default='/etc/buoy/logging.yaml', type=is_valid_file)
+
+    return parser.parse_args()
